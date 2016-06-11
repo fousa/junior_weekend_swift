9a264be6d1bfe0e6780b715cbd55e1db5cdf927e
Jelle Vandebeeck <jelle@fousa.be>
2016-06-11 09:43:02 +0200

    Setup the basic project.

---
+++ JuniorWeekend/JuniorWeekend/JuniorWeekend.h
@@ -0,0 +1,19 @@
+//
+//  JuniorWeekend.h
+//  JuniorWeekend
+//
+//  Created by Jelle Vandebeeck on 11/06/16.
+//  Copyright © 2016 Fousa. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for JuniorWeekend.
+FOUNDATION_EXPORT double JuniorWeekendVersionNumber;
+
+//! Project version string for JuniorWeekend.
+FOUNDATION_EXPORT const unsigned char JuniorWeekendVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JuniorWeekend/PublicHeader.h>
+
+
